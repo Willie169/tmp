9fe432762c5358306638f72232c2b3e1293938c1
Willie169 <cg10250207@gmail.com>
2025-02-03 14:46:52 +0800

    m

--- Shell, Linux, and Termux/android-studio.sh
+++ Shell, Linux, and Termux/android-studio.sh
@@ -7,7 +7,7 @@ cd android-studio/bin
 echo 'export PATH="$PREFIX/android-studio/bin:$PATH"' >> ~/.bashrc
 source ~/.bashrc
 ./studio.sh
-echo '[Desktop Entry]
+sudo echo '[Desktop Entry]
 Version=1.0
 Name=Android Studio
 Comment=Android Studio IDE
@@ -15,5 +15,5 @@ Exec=$PREFIX/android-studio/bin/studio.sh
 Icon=$PREFIX/android-studio/bin/studio.png
 Terminal=false
 Type=Application
-Categories=Development;IDE;' >> ~/.local/share/applications/android-studio.desktop
-chmod +x ~/.local/share/applications/android-studio.desktop
+Categories=Development;IDE;' >> ~/usr/share/applications/android-studio.desktop
+chmod +x ~/usr/share/applications/android-studio.desktop
