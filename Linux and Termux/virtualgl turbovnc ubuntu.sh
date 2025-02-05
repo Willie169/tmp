e0919ec998f906b02cf4db371158f7f695e6461d
Willie169 <cg10250207@gmail.com>
2025-02-05 22:21:18 +0800

    m

--- Linux and Termux/virtualgl turbovnc ubuntu.sh
+++ Linux and Termux/virtualgl turbovnc ubuntu.sh
@@ -8,27 +8,30 @@ rm turbovnc_3.1_amd64.deb*
 wget https://sourceforge.net/projects/libjpeg-turbo/files/3.0.1/libjpeg-turbo-official_3.0.1_amd64.deb
 sudo dpkg -i libjpeg-turbo-official_3.0.1_amd64.deb
 rm libjpeg-turbo-official_3.0.1_amd64.deb*
-sudo apt install libglu1-mesa mesa-utils -y
+sudo apt install libglu1-mesa mesa-utils xfce4 xfce4-goodies -y
+# Use default gdm3 display manager
 echo '#!/bin/sh
 unset SESSION_MANAGER
 unset DBUS_SESSION_BUS_ADDRESS
-[ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup
 [ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources
-export XKL_XMODMAP_DISABLE=1
-export XDG_CURRENT_DESKTOP="GNOME-Flashback:Unity"
-export XDG_MENU_PREFIX="gnome-flashback-"
-gnome-session --session=gnome-flashback-metacity --disable-acceleration-check
-' >> ~/.vnc/xstartup.turbovnc
+startxfce4 &' >> ~/.vnc/xstartup.turbovnc
 chmod +x ~/.vnc/xstartup.turbovnc
+vncpasswd
 sudo vglserver_config
+# 1, yes
 sudo usermod --groups vglusers root
 sudo usermod --groups vglusers $USER
 mkdir -p /etc/opt/VirtualGL
-chgrp vglusers /etc/opt/VirtualGL
-chmod 750 /etc/opt/VirtualGL
+sudo chgrp vglusers /etc/opt/VirtualGL
+sudo chmod 750 /etc/opt/VirtualGL
+## For Nvidia
+## Use tty or ssh client
+# sudo systemctl stop gdm
+# sudo modprobe -r nvidia_uvm nvidia_drm nvidia_modeset nvidia
+# sudo systemctl start gdm
 ## Log out and log in
 # xauth merge /etc/opt/VirtualGL/vgl_xauth_key
-# xdpyinfo -display :0
+# xdpyinfo -display :1
 ## Below is roughly the same as vncserver of tigervnc
 # /opt/TurboVNC/bin/vncserver
 ## Source: https://www.google.com/amp/s/blog.gtwang.org/linux/nvidia-tesla-p40-virtualgl-vnc-remote-3d-rendering-server-installation/amp
