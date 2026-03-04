b54c4fc66ffe604129618c81095161583be4b1b6
Willie169 <cg10250207@gmail.com>
2026-03-04 12:06:47 +0800

    l

--- ollama.sh
+++ ollama.sh
@@ -2,5 +2,4 @@ ollama pull hf.co/unsloth/gpt-oss-20b-GGUF:Q4_K_M
 ollama pull qwen3.5:9b-q4_K_M
 ollama pull qwen3.5:4b-q4_K_M
 ollama pull qwen3.5:2b-q4_K_M
-ollama pull qwen3-coder-next:q4_K_M
 ollama pull nomic-embed-text:v1.5
