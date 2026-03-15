6db3a791a6a27e11690c0b01c33a6268e0433008
Willie169 <cg10250207@gmail.com>
2026-03-15 21:09:46 +0800

    a

--- Windows11VM.sh
+++ Windows11VM.sh
@@ -7,8 +7,8 @@ if [ ! -f "$$HOME/iso/Win11_25H2_EnglishInternational_x64.iso" ]; then
   echo "$HOME/iso/Win11_25H2_EnglishInternational_x64.iso not found" >&2
   exit 1
 fi
-mkdir -p "win11_vm/tpm"
-cp /usr/share/OVMF/OVMF_VARS_4M.ms.fd ~/win11_vm/OVMF_VARS_4M.ms.fd
+mkdir -p "$HOME/win11_vm/tpm"
+cp /usr/share/OVMF/OVMF_VARS_4M.ms.fd "$HOME/win11_vm/OVMF_VARS_4M.ms.fd"
 wget https://fedorapeople.org/groups/virt/virtio-win/direct-downloads/stable-virtio/virtio-win.iso -O "$HOME/iso/virtio-win.iso"
 sudo tee  /etc/apt/sources.list.d/swtpm.list<<EOF
 deb [trusted=yes] https://ppa.launchpadcontent.net/stefanberger/swtpm-noble/ubuntu noble main
@@ -26,7 +26,7 @@ swtpm_setup \
 swtpm socket --tpm2 \
   --tpmstate dir="$HOME/win11_vm/tpm" \
   --ctrl type=unixio,path="/tmp/swtpm-sock" &
-sudo qemu-system-x86_64 \
+qemu-system-x86_64 \
   -enable-kvm \
   -cpu host \
   -m 4G \
@@ -35,7 +35,7 @@ sudo qemu-system-x86_64 \
   -drive if=pflash,format=raw,readonly=on,file=/usr/share/OVMF/OVMF_CODE_4M.ms.fd \
   -drive if=pflash,format=raw,file="$HOME/win11_vm/OVMF_VARS_4M.ms.fd" \
   -drive file="$HOME/qcow2base/win11.qcow2",if=virtio \
-  -cdrom "$$HOME/iso/Win11_25H2_EnglishInternational_x64.iso" \
+  -cdrom "$HOME/iso/Win11_25H2_EnglishInternational_x64.iso" \
   -netdev user,id=n1,hostfwd=tcp::3222-:22 \
   -device virtio-net-pci,netdev=n1 \
   -display sdl \
